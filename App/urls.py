8c399924ee0655e6f2146245fc202ad20df11f7d
kebasoh <ongatikebasoba@gmail.com>
2020-02-16 14:28:24 +0300

    addedd the app files

---
+++ App/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views 
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('profile/<int:id>/',views.profile,name='profile'), 
+    path('',views.review_form,name='review' ),
+    path('search/', views.search_results, name='search_results') 
+    
+    
+    
+
+    
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
